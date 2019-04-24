519d4a046fd974c826d7cc0cd5ddc073b2373b74
Dana Simmons <dana@simmons-audio.com>
2019-04-24 17:11:27 -0400

    claims and offers get destroyed when pantry items are deleted

--- app/models/pantry_item.rb
+++ app/models/pantry_item.rb
@@ -1,4 +1,4 @@
 class PantryItem < ApplicationRecord
     belongs_to :user
-    has_many :claims
+    has_many :claims, dependent: :destroy
 end
--- app/models/pantry_request.rb
+++ app/models/pantry_request.rb
@@ -1,4 +1,4 @@
 class PantryRequest < ApplicationRecord
     belongs_to :user
-    has_many :offers
+    has_many :offers, dependent: :destroy
 end
