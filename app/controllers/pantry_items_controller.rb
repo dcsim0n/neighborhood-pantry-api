ab9124519c0ffc8a77f1be6050857b323155399f
Dana Simmons <dana@simmons-audio.com>
2019-04-25 13:43:32 -0400

    defined create index and update actions for pantry items

--- app/controllers/pantry_items_controller.rb
+++ app/controllers/pantry_items_controller.rb
@@ -1,2 +1,40 @@
 class PantryItemsController < ApplicationController
+    def index
+        #add authorization here
+        items = PantryItem.where(user_id: params[:user_id])
+        render json: items, status: :ok
+    end
+    def create
+        
+        user = current_user
+        item_attributes = pantry_item_params
+        #get the user id from the url
+        item_attributes = item_attributes.merge(user_id: params[:user_id])
+        #add authorization here
+        item = PantryItem.new(item_attributes)
+        if item.valid?
+            item.save
+            render json: user.pantry_items, status: :ok
+        else
+            render json: {erros: item.erros.full_messages}, status: :unprocessable_entity 
+        end
+        
+    end
+
+    def delete
+        item = PantryItem.find(params[:id])
+        if current_user.id === item.user_id
+            item.destroy
+            render status: :ok
+        else
+            render json: {errors: "Unathorized Action", status: :unauthorized}
+        end
+    end
+
+
+    private
+    def pantry_item_params
+        item = params.require(:pantry_item).permit(:name, :quantity, :unit)
+        
+    end
 end
--- spec/requests/users controllers_spec.rb
+++ spec/requests/users controllers_spec.rb
@@ -5,10 +5,10 @@ RSpec.describe "Users controllers", type: :request do
     it "nested resources can be created and returned" do
       address = {street: "", city: "", state: "", zip:""}
       user = User.create!(first_name: "test", last_name:"test", email: "test@test.com", password:"test", address_attributes: address)
-      
+      token = JWT.encode({id: user.id}, Rails.application.credentials.jwt_secret, 'HS512')
       it "can create a pantry_item as a nested resource"do 
         item = {name:"Froot by the foot", quantity:10, units:"roll"}
-        body = {token: }
+        body = {token: token, pantry_item: item }
 
       end
       
