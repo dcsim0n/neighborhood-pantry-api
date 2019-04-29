e9d5b8ca7c4e935fb2f5c3cc5210c058b808c867
Dana Simmons <dana@simmons-audio.com>
2019-04-29 18:15:07 -0400

    removed authorization for neighborhood reading

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -4,4 +4,28 @@ class NeighborhoodsController < ApplicationController
         #authorize! :read, hoods.first
         render json: hoods, status: :ok
     end
+
+    def search
+        @user = User.find(params[:user_id])
+        @neighborhoods = Neighborhood.all.select {|hood| hood.distance_to(@user) <= params[:radius].to_i}
+        render json: @neighborhoods, status: :ok
+    end
+    def find
+        places = Geocoder.search(params[:q])
+        render json: places.as_json, status: :ok
+    end
+
+    def create
+        @neighborhood = Neighborhood.new(neighborhood_params)
+        if @neighborhood.valid?
+            @neighborhood.save
+            render json: @neighborhood, status: :ok
+        else
+            render json: {errors: @neighborhood.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    def neighborhood_params
+        params.permit(:name, :latitude, :longitude)
+    end
 end
