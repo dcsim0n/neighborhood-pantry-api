402f68515812f02bb140ba41e8016cfd9a346184
Dana Simmons <dana@simmons-audio.com>
2019-04-29 14:40:16 -0400

    problem authorizing an empty array of neighborhoods

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -1,7 +1,7 @@
 class NeighborhoodsController < ApplicationController
     def index
         hoods = Neighborhood.joins(:neighbors).where(neighbors: {user_id: params[:user_id]})
-        authorize! :read, hoods.first
+        #authorize! :read, hoods.first
         render json: hoods, status: :ok
     end
 end
