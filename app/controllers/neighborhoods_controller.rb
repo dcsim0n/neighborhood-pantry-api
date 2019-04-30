8ce2ebbfa31468317e4f38f8a0f206c113eee2b9
Dana Simmons <dana@simmons-audio.com>
2019-04-30 13:57:58 -0400

    neighborhood show routeshow route returns all the requests, items, and user info for that neighborhood

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -5,6 +5,18 @@ class NeighborhoodsController < ApplicationController
         render json: hoods, status: :ok
     end
 
+    def show
+        neighborhood = Neighborhood.find(params[:id])
+        authorize! :read, neighborhood
+        requests = neighborhood.pantry_requests.includes(:user)
+        items = neighborhood.pantry_items.includes(:user)
+        #at some point maybe the address should be here too?
+        #also we will eventually want to include the offers and claims here
+        json_requests = requests.as_json({:include =>{:user=>{only:[:id,:first_name,:last_name]}}})
+        json_items = items.as_json({:include =>{:user=>{only:[:id,:first_name,:last_name]}}})
+        render json: {requests: json_requests, items: json_items}, status: :ok
+    end
+
     def search
         @user = User.find(params[:user_id])
         @neighborhoods = Neighborhood.all.select {|hood| hood.distance_to(@user) <= params[:radius].to_i}
@@ -24,7 +36,7 @@ class NeighborhoodsController < ApplicationController
             render json: {errors: @neighborhood.errors.full_messages}, status: :unprocessable_entity
         end
     end
-
+    private
     def neighborhood_params
         params.permit(:name, :latitude, :longitude)
     end
--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,6 +1,7 @@
 class Neighborhood < ApplicationRecord
     has_many :neighbors, dependent: :destroy
     has_many :users, through: :neighbors
-
+    has_many :pantry_requests, through: :users
+    has_many :pantry_items, through: :users
     reverse_geocoded_by :latitude, :longitude
 end
