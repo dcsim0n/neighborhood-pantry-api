512844a2392a08511f9440852b419f6175e3cd51
Dana Simmons <dana@simmons-audio.com>
2019-05-01 11:54:36 -0400

    defined show route for pantry items, could probably put these in the user-resource-controller

--- app/controllers/pantry_items_controller.rb
+++ app/controllers/pantry_items_controller.rb
@@ -1,6 +1,10 @@
 class PantryItemsController < UserResourcesController
 
-
+    def show
+        item = PantryItem.find(params[:id])
+        authorize! :read, item
+        render json: item.as_json({include: [{user: {only:[:id, :first_name]}},:claims]}), status: :ok
+    end
     private
     def find_resource
         PantryItem.find(params[:id])
