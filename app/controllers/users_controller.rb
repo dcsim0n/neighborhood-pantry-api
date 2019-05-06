b73073b161ff6df7242b93c86561f44de1157e06
Dana Simmons <dana@simmons-audio.com>
2019-05-06 13:52:58 -0400

    user stuff returns an address

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,14 @@ class UsersController < ApplicationController
 
     def show
         @user = User.find(params[:id])
-        user_info = @user.as_json(only: [:id, :email, :first_name, :last_name])
-        render json: user_info, status: :ok
+        render json: user_info(@user), status: :ok
     end
     def create
         @user = User.new(user_params)
         if(@user.valid?)
             @user.save
             token = user_token(@user)
-            user_info = @user.as_json(only: [:id, :email, :first_name, :last_name])
-            render json: {token: token, user: user_info}, status: :ok
+            render json: {token: token, user: user_info(@user)}, status: :ok
         else
             render json: {errors: @user.errors.full_messages}, status: :unprocessable_entity
         end
@@ -23,14 +21,16 @@ class UsersController < ApplicationController
         
         if @user && @user.authenticate(params[:password])
             token = user_token(@user)
-            user_info = @user.as_json(only: [:id, :email, :first_name, :last_name])
-            render json: {token: token, user: user_info }, status: :ok
+            render json: {token: token, user: user_info(@user) }, status: :ok
         else
             render json: {errors: ["Invalid email or password"]}, status: :unauthorized
         end
     end
 
     private
+    def user_info(user)
+        user_info = @user.as_json(only: [:id, :email, :first_name, :last_name],include: :address)
+    end
     def user_params
         params.permit(
             :first_name,
